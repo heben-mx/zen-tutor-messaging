a59aaae85f7141901d791c76f2d6c1f83cf99f81
Alfredo Dominguez <a.lucio@samsung.com>
2024-08-08 20:09:02 -0300

    ec2 script

---
+++ init.bash
@@ -0,0 +1,9 @@
+sudo su
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 22
+yum update -y
+yum install git -y
+git clone https://github.com/heben-mx/zen-tutor-messaging.git
+cd zen-tutor-messaging
+npm i && npm start
